70bb6163300e5a1a1b3543d1fe47a27a77e8581f
XiaoFeng Gong <20091570@wit.ie>
2020-10-16 14:17:29 +0100

    Tested findProductById

--- test/catalogue-test.js
+++ test/catalogue-test.js
@@ -3,6 +3,7 @@ const Catalogue = require("../src/productCatalogue");
 const Product = require("../src/product");
 const expect = chai.expect;
 
+
 let cat = null;
 let batch = null;
 
@@ -13,6 +14,7 @@ cat.addProduct(new Product("A124", "Product 2", 100, 10.0));
 cat.addProduct(new Product("A125", "Product 3", 100, 10, 10.0));
 
 describe("Catalogue", () => {
+  //
   describe("findProductById", function () {
     it("should find a valid product id", function () {
       const result = cat.findProductById("A123");
